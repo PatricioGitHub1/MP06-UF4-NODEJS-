9c063e81b848f6cfb676c922b3ad0b697c9e3ccd
PatricioGitHub1 <rojas_patricio_github@gmx.es>
2024-04-28 22:08:20 +0200

    finished nodejs activities

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dbConfig = require('./config/db');
 const userRoutes = require('./api/routes/userRoutes');
+const eventRoutes = require('./api/routes/eventRoutes');
 const Event = require('./api/models/event');
 const app = express();
 
@@ -13,36 +14,8 @@ mongoose.connect(dbConfig.MONGODB_URI).then(() => console.log("Connected to Mong
   .catch(err => console.error("Could not connect to MongoDB", err));
 
 
-// Enpoint GET simplet que retorna "OK"
-app.get('/api/health', (req, res) => {
-  res.json({ status: "OK" });
-});
-
+app.use('/api', eventRoutes);
 app.use('/api', userRoutes);
-
-// POST endpoint per inserir un esdeveniment
-app.post('/api/events', async (req, res) => {
-  try {
-    const event = new Event(req.body);
-    await event.save();
-    res.status(201).send(event);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
-
-// Endpoint per recuperar un esdeveniment per ID
-app.get('/api/events/:id', async (req, res) => {
-  try {
-    const event = await Event.findById(req.params.id);
-    if (!event) {
-      return res.status(404).send("L'esdeveniment no s'ha trobat.");
-    }
-    res.send(event);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
 
 // Exporta l'instància de l'aplicació perquè pugui ser utilitzada en altres llocs, com els tests.
 module.exports = app;
